b115c41cd1ab41bd1ab67917ea5ff6fac4d7f58f
JonahY <48706025+JonahY@users.noreply.github.com>
2020-10-22 15:26:03 +0800

    update

--- preprocess.py
+++ preprocess.py
@@ -11,8 +11,11 @@ import time
 
 
 class Preprocessing:
-    def __init__(self, thr_dB, thr_noise_ratio):
-        self.thr_V = pow(10, thr_dB / 20) / (thr_noise_ratio * pow(10, 6))
+    def __init__(self, thr_dB, thr_noise_ratio, magnification_dB):
+        self.thr_dB = thr_dB
+        self.thr_noise_ratio = thr_noise_ratio
+        self.magnification_dB = magnification_dB
+        self.thr_V = pow(10, self.thr_dB / 20) / (self.thr_noise_ratio * pow(10, 6))
         self.counts = 1
         self.duration = 0
         self.amplitude = 0
@@ -23,13 +26,13 @@ class Preprocessing:
         self.time = 0
         self.channel_num = 0
         self.sample_interval = 0
+        self.magnification = pow(10, self.magnification_dB / 20)
 
     def skip_n_column(self, file, n=3):
         for _ in range(n):
             file.readline()
 
-    def cal_features(self, dataset, time_label):
-        valid_wave_idx = np.where(abs(dataset) >= self.thr_V)[0]
+    def cal_features(self, dataset, time_label, valid_wave_idx):
         start = time_label[valid_wave_idx[0]]
         end = time_label[valid_wave_idx[-1]]
         self.duration = end - start
@@ -45,7 +48,7 @@ class Preprocessing:
     def cal_counts(self, valid_data):
         N = len(valid_data)
         for idx in range(1, N):
-            if valid_data[idx - 1] <= thr_V <= valid_data[idx]:
+            if valid_data[idx - 1] <= self.thr_V <= valid_data[idx]:
                 self.counts += 1
 
     def main(self, file_name, data=[]):
@@ -59,19 +62,24 @@ class Preprocessing:
                 self.channel_num = int(f.readline().strip()[16:])
                 self.hit_num = int(f.readline()[12:])
                 self.time = float(f.readline()[14:])
-                dataset = np.array([float(i.strip("\n")) for i in f.readlines()[1:]])
+                dataset = np.array([float(i.strip("\n")) for i in f.readlines()[1:]]) / self.magnification
                 time_label = np.linspace(self.time, self.time + self.sample_interval * (points_num - 1), points_num)
 
                 # calculate the duration, amplitude, rise_time, energy and counts
-                valid_data = self.cal_features(dataset, time_label)
-                self.cal_counts(valid_data)
-                if self.counts >= 2:
-                    data.append('{}, {:.7f}, {}, {:.2f}, {:.2f}, {:.7f}, {:.7f}, {:.2f}, {:.2f}, {}\n'.format(
-                        self.hit_num, self.time, self.channel_num, self.thr_V * pow(10, 6),
-                        self.amplitude * pow(10, 6), self.rise_time, self.duration,
-                        self.energy * pow(10, 14), self.RMS * pow(10, 6), self.counts))
+                valid_wave_idx = np.where(abs(dataset) >= self.thr_V)[0]
+                # print(dataset[0], dataset[-1], len(dataset))
+                # print(valid_wave_idx)
+
+                if valid_wave_idx.shape[0]:
+                    valid_data = self.cal_features(dataset, time_label, valid_wave_idx)
+                    self.cal_counts(valid_data)
+                    if self.counts >= 2:
+                        data.append('{}, {:.7f}, {}, {:.8f}, {:.1f}, {:.8f}, {:.1f}, {:.7f}, {:.7f}, {:.8f}, {:.8f}, {}\n'.format(
+                            self.hit_num, self.time, self.channel_num, self.thr_V * pow(10, 6), self.thr_dB,
+                            self.amplitude * pow(10, 6), 20 * np.log10(self.thr_noise_ratio * self.amplitude * pow(10, 6)),
+                            self.rise_time, self.duration, self.energy * pow(10, 14), self.RMS * pow(10, 6), self.counts))
             pbar.set_description("Calculating: %s" % name.split('_')[2])
-            # ID, Time(s), Chan, Thr(μV), Amp(μV), RiseT(s), Dur(s), Eny(aJ), RMS(μV), Counts
+            # ID, Time(s), Chan, Thr(μV), Thr(dB), Amp(μV), Amp(dB), RiseT(s), Dur(s), Eny(aJ), RMS(μV), Counts
             # print("-" * 50)
             # print(self.hit_num, self.time * pow(10, 6), self.channel_num, self.thr_V * pow(10, 6), self.amplitude * pow(10, 6), self.rise_time * pow(10, 6), self.duration * pow(10, 6), self.energy * pow(10, 14), self.RMS * pow(10, 6), self.counts)
 
@@ -82,7 +90,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, default="C:\\Users\\Yuan\\Desktop\\CM-6M-o18-2020.10.17-1-60-a-waveform", help="Path of data")
     parser.add_argument("--thr_dB", type=int, default=25, help="Detection threshold")
-    parser.add_argument("--thr_noise_ratio", type=int, default=5, help="Cpu core number")
+    parser.add_argument("--thr_noise_ratio", type=int, default=5, help="Threshold to noise ratio")
+    parser.add_argument("--magnification_dB", type=int, default=60, help="Magnification /dB")
     parser.add_argument("--threads", type=int, default=4, help="Cpu core number")
     opt = parser.parse_args()
     print(opt)
@@ -97,18 +106,18 @@ if __name__ == "__main__":
     # Multithreading acceleration
     pool = multiprocessing.Pool(processes=opt.threads)
     for i in range(0, len(file_list), each_core):
-        process = Preprocessing(opt.thr_dB, opt.thr_noise_ratio)
+        process = Preprocessing(opt.thr_dB, opt.thr_noise_ratio, opt.magnification_dB)
         result.append(pool.apply_async(process.main, (file_list[i:i + each_core],)))
 
     txt_name = opt.data_path.split('\\')[-1] + '.txt'
     f = open(txt_name, "w")
-    f.write("ID, Time(s), Chan, Thr(μV), Amp(μV), RiseT(s), Dur(s), Eny(aJ), RMS(μV), Counts\n")
+    f.write("ID, Time(s), Chan, Thr(μV), Thr(dB), Amp(μV), Amp(dB), RiseT(s), Dur(s), Eny(aJ), RMS(μV), Counts\n")
     pbar = tqdm(result)
     for idx, i in enumerate(pbar):
         tmp = i.get()
         data += tmp
         pbar.set_description("Exporting Data: {}/{}".format(idx + 1, opt.threads))
-    data = sorted(data, key=lambda s: int(s.split(',')[0]))
+    data = sorted(data, key=lambda s: float(s.split(',')[0]))
     for i in data:
         f.write(i)
     f.close()
