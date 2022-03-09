e2cb1c9ef090f1ada6423ec5c51c4a7dbf4729c3
JonahY <binchengy@163.com>
2022-03-09 14:04:35 +0800

    add exception catch

--- run_remote/addTriggerTime.py
+++ run_remote/addTriggerTime.py
@@ -7,28 +7,33 @@ import multiprocessing
 from multiprocessing import cpu_count
 import math
 import argparse
+import traceback
 
 
 def cut_stream(files, streamFold, saveFold):
-    pbar = tqdm(files, ncols=100)
-    for file in pbar:
-        pbar.set_description('File name: %s' % file[43:-4])
+    try:
+        pbar = tqdm(files, ncols=100)
+        for file in pbar:
+            pbar.set_description('File name: %s' % file[43:-4])
 
-        with open(os.path.join(streamFold, file), 'r') as f:
-            for _ in range(7):
-                f.readline()
-            trigger_time = float(f.readline().strip()[15:])
+            with open(os.path.join(streamFold, file), 'r') as f:
+                for _ in range(7):
+                    f.readline()
+                trigger_time = float(f.readline().strip()[15:])
 
-        for j in os.listdir(saveFold):
-            if file[43:-4] in j:
-                with open(os.path.join(saveFold, j), "r+") as f:
-                    old = f.read()
-                    f.seek(0)
-                    f.write('Trigger time (s)\n%.8f\n\n' % trigger_time)
-                    f.write(old)
+            for j in os.listdir(saveFold):
+                if file[43:-4] in j:
+                    with open(os.path.join(saveFold, j), "r+") as f:
+                        old = f.read()
+                        f.seek(0)
+                        f.write('Trigger time (s)\n%.8f\n\n' % trigger_time)
+                        f.write(old)
 
-                with open(os.path.join(saveFold, 'log'), 'a') as f:
-                    f.write('%s\n' % file)
+                    with open(os.path.join(saveFold, 'log'), 'a') as f:
+                        f.write('%s\n' % file)
+    except Exception as e:
+        print('Error: %s' % e)
+        print(traceback.format_exc())
 
 
 if __name__ == '__main__':
