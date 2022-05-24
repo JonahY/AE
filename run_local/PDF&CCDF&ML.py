f57802d7d963689a458485b65b2bb90c580de3b3
JonahY <binchengy@163.com>
2022-05-24 18:33:18 +0800

    update

--- run_local/PDF&CCDF&ML.py
+++ run_local/PDF&CCDF&ML.py
@@ -2,7 +2,7 @@
 from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+from math import floor, ceil
 from tqdm import tqdm
 from matplotlib.pylab import mpl
 import warnings
@@ -87,8 +87,8 @@ class Features:
         :param tmp: Energy/Amplitude/Duration in order of magnitude
         :return:
         """
-        tmp_min = math.floor(np.log10(min(tmp)))
-        tmp_max = math.ceil(np.log10(max(tmp)))
+        tmp_min = floor(np.log10(min(tmp)))
+        tmp_max = ceil(np.log10(max(tmp)))
         inter = [i for i in range(tmp_min, tmp_max + 1)]
         return inter
 
@@ -361,35 +361,35 @@ class Features:
         plot_norm(ax, xlabel, ylabel, y_lim=[1.3, 2.6], legend_loc='upper right')
 
 
-# if __name__ == '__main__':
-#     # 读取txt文件中的数据
-#     # 数据的绝对路径，注意与转义符加以区别
-#     dirPath = ''
-#     with open(dirPath, 'r') as f:
-#         data = np.array(list(map(lambda i: float(i.strip()), f.readlines())))
-#
-#     # 按步长生成时间序列
-#     # 时间步长
-#     timeStep = 0.001
-#     t = np.linspace(0, timeStep * (data.shape[0] - 1), data.shape[0])
-#
-#     # 设置颜色为了区分不同类别的数据
-#     color_1 = [255 / 255, 0 / 255, 102 / 255]  # red
-#     color_2 = [0 / 255, 136 / 255, 204 / 255]  # blue
-#
-#     # 图注
-#     status = ''
-#
-#     # 由于求导后存在为0的数据，此处将其替换为非零值（任意无穷小的数）
-#     replaceZero = 1e-6
-#     data[np.where(data == 0)[0]] = replaceZero
-#
-#     features = Features(color_1, color_2, t, status)
-#     # PDF
-#     features.cal_PDF(sorted(data), sorted(data), sorted(data), 'Energy (aJ)', 'PDF (E)',
-#                      [[0, None], [0, None], [0, None]], [8, 8, 8], bin_method='log', select=[0, 1], FIT=True)
-#     # CCDF
-#     features.cal_CCDF(sorted(data), sorted(data), sorted(data), 'Energy (aJ)', 'CCD C(s)',
-#                       [[0, float('inf')], [0, float('inf')], [0, float('inf')]], select=[0, 1], FIT=True)
-#     # ML 该程序计算复杂度有待优化，若数据量较大，可在原有数据基础上间隔取值计算ML
-#     features.cal_ML(sorted(data), sorted(data), sorted(data), 'Energy (aJ)', 'ML (E)', select=[0, 1])
+if __name__ == '__main__':
+    # 读取txt文件中的数据
+    # 数据的绝对路径，注意与转义符加以区别
+    dirPath = ''
+    with open(dirPath, 'r') as f:
+        data = np.array(list(map(lambda i: float(i.strip()), f.readlines())))
+
+    # 按步长生成时间序列
+    # 时间步长
+    timeStep = 0.001
+    t = np.linspace(0, timeStep * (data.shape[0] - 1), data.shape[0])
+
+    # 设置颜色为了区分不同类别的数据
+    color_1 = [255 / 255, 0 / 255, 102 / 255]  # red
+    color_2 = [0 / 255, 136 / 255, 204 / 255]  # blue
+
+    # 图注
+    status = ''
+
+    # 由于求导后存在为0的数据，此处将其替换为非零值（任意无穷小的数）
+    replaceZero = 1e-6
+    data[np.where(data == 0)[0]] = replaceZero
+
+    features = Features(color_1, color_2, t, status)
+    # PDF
+    features.cal_PDF(sorted(data), sorted(data), sorted(data), 'Energy (aJ)', 'PDF (E)',
+                     [[0, None], [0, None], [0, None]], [8, 8, 8], bin_method='log', select=[0, 1], FIT=True)
+    # CCDF
+    features.cal_CCDF(sorted(data), sorted(data), sorted(data), 'Energy (aJ)', 'CCD C(s)',
+                      [[0, float('inf')], [0, float('inf')], [0, float('inf')]], select=[0, 1], FIT=True)
+    # ML 该程序计算复杂度有待优化，若数据量较大，可在原有数据基础上间隔取值计算ML
+    features.cal_ML(sorted(data), sorted(data), sorted(data), 'Energy (aJ)', 'ML (E)', select=[0, 1])
